4aa75b72fefecbd8a3da0db18761b60190e8e9fb
pavolhodas <pavolhodas4@gmail.com>
2020-10-05 08:11:02 +0200

    Add implemetation area off triangle

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -5,9 +5,8 @@ namespace Open_Lab_01._04
     public class Triangle
     {
         public double Area(double foundation, double height)
-        {
-            double area = (foundation * height)/ 2;
-            return area ;
+        { 
+            return (foundation * height) / 2;
         }
     }
 }
