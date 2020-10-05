02f8c896e911755a401d4651e6e9532d30256153
pavolhodas <pavolhodas4@gmail.com>
2020-10-05 07:25:51 +0200

    Area

--- Open-Lab-01.04/Triangle.cs
+++ Open-Lab-01.04/Triangle.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_01._04
     {
         public double Area(double foundation, double height)
         {
-            throw new NotImplementedException();
+            double i = 5;
+            double b = 4;
+            double obsah = Area( i,  b);
+            return obsah;
         }
     }
 }
